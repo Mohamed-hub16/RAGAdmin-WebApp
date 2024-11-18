1363eb6a9594fd4d486389dd0046602708dc4601
Matias DEVEZE <matiasdeveze@gmail.com>
2024-11-18 10:48:05 +0100

    feat: add history to see old discussions

--- rag-webapp/src/components/main-interface/Main.tsx
+++ rag-webapp/src/components/main-interface/Main.tsx
@@ -1,6 +1,7 @@
 import Header from "./header/Header";
 import {Alerts} from "./alerts/Alerts";
 import "../../css/main-interface/main.css"
+import {Historical} from "./historical/Historical";
 
 export function Main() {
     return (
@@ -8,6 +9,7 @@ export function Main() {
             <Header/>
             <div className="left-side">
                 <Alerts/>
+                <Historical/>
             </div>
         </>
     );
