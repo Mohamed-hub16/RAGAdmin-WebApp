b9e17a369384805095f36e0b516720e653d45715
Lacroix <enzo.lacroix@etu.unilim.fr>
2024-11-28 14:01:34 +0100

    feat : front register page

---
+++ rag-webapp/src/components/register-interface/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ragadminLogo from "../../res/ragadmin-final.png"
+import laPosteLogo from "../../res/laposte-logo.png"
+import '../../css/register-interface/header.css';
+
+const Header: React.FC = () => {
+    return(
+        <header className="header">
+            <img src={laPosteLogo} alt="La Poste" className="logo-la-poste"/>
+            <img src={ragadminLogo} alt="RAGAdmin" className="logo-RAGAdmin"/>
+        </header>
+
+    );
+};
+
+export default Header;
