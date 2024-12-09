01a62487239f496e9edd7a4317a167fc36818dcb
Matias DEVEZE <matiasdeveze@gmail.com>
2024-12-09 16:27:17 +0100

    feat: route api for delete a chat

--- backend/routes/historicals.js
+++ backend/routes/historicals.js
@@ -1,9 +1,10 @@
 import { getHistoricalsByUserId } from "../sqlite/historical/getHistoricalsByUserId.js";
 import express from "express";
+import {deleteHistorical} from "../sqlite/historical/deleteHistorical.js";
 
 const router = express.Router();
 
-export default router.get('/:userId', async (req, res) => {
+router.get('/:userId', async (req, res) => {
     const { userId } = req.params;
 
     console.log('Fetching historicals for userId:', userId);
@@ -24,3 +25,27 @@ export default router.get('/:userId', async (req, res) => {
         res.status(500).json({ message: 'Erreur lors de la récupération des historiques.', error: err.message });
     }
 });
+
+router.delete('/:historicalId', async (req, res) => {
+    const { historicalId } = req.params;
+
+    console.log('Deleting historical with ID:', historicalId);
+
+    try {
+        const success = await deleteHistorical(historicalId);
+
+        if (success) {
+            console.log(`Historique avec l'ID ${historicalId} supprimé avec succès.`);
+            return res.status(200).json({ message: `Historique avec l'ID ${historicalId} supprimé avec succès.` });
+        } else {
+            console.log(`Aucun historique trouvé avec l'ID ${historicalId}`);
+            return res.status(404).json({ message: `Aucun historique trouvé avec l'ID ${historicalId}.` });
+        }
+    } catch (err) {
+        console.error('Erreur lors de la suppression de l’historique:', err);
+        res.status(500).json({ message: 'Erreur lors de la suppression de l\'historique.', error: err.message });
+    }
+});
+
+export default router;
+
